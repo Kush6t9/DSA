fe5bd5d070fb51c7a54ef24c2e703d43f81db54d
Kush Kumar <kkushkumar3001@gmail.com>
2026-07-27 19:26:51 +0530

    Time: 50 ms (76.33%) | Memory: 178 MB (79.38%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,44 +1,18 @@
 class Solution {
 public:
-    vector<int> nextsmallestelement(vector<int> arr){
-        int n=arr.size();
-        vector<int> ans(n);
-        stack<int> stk;
-        for(int i=n-1;i>=0;i--){
-            while(!stk.empty() && arr[i]<=arr[stk.top()]){
-                stk.pop();
-            }
-            if(stk.empty()) ans[i]=n;
-            else ans[i]=stk.top();
-            stk.push(i);
-        }
-        return ans;
-    }
-    vector<int> previossmallestelement(vector<int> arr){
-        int n=arr.size();
-        vector<int> ans(n);
-        stack<int> stk;
-        for(int i=0;i<n;i++){
-            while(!stk.empty() && arr[stk.top()]>arr[i]){
-                stk.pop();
-            }
-            if(stk.empty()) ans[i]=-1;
-            else ans[i]=stk.top();
-            stk.push(i);
-        }
-        return ans;
-    }
-
     int sumSubarrayMins(vector<int>& arr) {
-        vector<int> NSE,PSEE;
-        NSE=nextsmallestelement(arr);
-        PSEE=previossmallestelement(arr);
+        stack<int> stk;
         long long total=0;
         int mod=1e9+7;
-        for(int i=0;i<arr.size();i++){
-            int nse=NSE[i]-i;
-            int psee=i-PSEE[i];
-            total=(total + (1LL * nse * psee * arr[i])%mod)%mod;
+        for(int i=0;i<=arr.size();i++){
+            while(!stk.empty() && (i==arr.size() || arr[stk.top()]>arr[i])){
+                int mid=stk.top();
+                stk.pop();
+                int l=stk.empty()?-1:stk.top();
+                int r=i;
+                total=(total + (1LL*(mid-l)*(r-mid)*arr[mid]))%mod;
+            }
+            stk.push(i);
         }
         return total;
     }
